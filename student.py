e7afb689a2e065a29d4f1c85c5281dbc5678e202
keerthimellihalli <keerthimellihalli@gmail.com>
2025-11-22 10:01:15 +0530

    student.py
    Fix indentation and correct usage message formatting.

---
+++ student.py
@@ -0,0 +1,11 @@
+import sys
+    #check if correct number of argv
+if len(sys.argv)! = 3:
+print("Usage: python student.py <name><rollno>")
+    #sys.argv[0] is always the program name
+script_name = sys.argv[0]
+rollno = sys.argv[2]
+
+print("script name:",script_name)
+print("student name:",name)
+print("rollno number:",rollno)
